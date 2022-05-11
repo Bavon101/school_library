0c2a0cb108f83134024eea65da92a857dde7b6cf
Bavon101 <bavonfate@gmail.com>
2022-05-11 16:52:38 +0300

    Create a relationship between Person and Rental

--- person.rb
+++ person.rb
@@ -8,10 +8,11 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   private
 
